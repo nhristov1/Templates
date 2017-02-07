307b48aa8e57a5e602081a239bfdec56db1ea712
Nikolay Hristov <nhristov@tycoint.com>
2017-02-07 15:23:00 +0000

    MVVM: Added UserControl and Relay Button in Prism it is called DispatchButton.

---
+++ MVVM/ViewModels/RelayButton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM.ViewModels
+{
+    class RelayButton : ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+        private readonly Action<object> _executeWithParameter; 
+
+        public RelayButton(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute; 
+            _canExecute = canExecute; 
+        }
+
+        public RelayButton(Action<object> execute, Func<bool> canExecute = null)
+        {
+            _executeWithParameter = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value;  }
+            remove { CommandManager.RequerySuggested -= value;  }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(); 
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+                _execute();
+            else if (_executeWithParameter != null)
+                _executeWithParameter(parameter); 
+        }
+    }
+}
