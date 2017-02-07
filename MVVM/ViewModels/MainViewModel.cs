3985e73adaf282aba13e68db929d79c55f0faf5b
Nikolay Hristov <nhristov@tycoint.com>
2017-02-07 16:46:14 +0000

    MVVM: Added Properties and Commands.

---
+++ MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MVVM.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private string _timeAndDateNow;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand PressUpdateTime { get; set; }
+
+        public MainViewModel()
+        {
+            PressUpdateTime = new RelayButton(DoUpdateTime); 
+        }
+
+        private void DoUpdateTime()
+        {
+            TimeAndDate = DateTime.Now.ToString(); 
+        }
+
+        public string TimeAndDate
+        { get
+            {
+                return _timeAndDateNow;
+            }
+            set
+            {
+                _timeAndDateNow = value;
+                NotifyPropertyChanged(); 
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName]String info = null)
+        {
+            if (info != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(info)); 
+        }
+    }
+}
